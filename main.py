4853e435e16456e729f254940f8518bbdfc22c39
William Miller <wmilleiv@gmail.com>
2025-10-22 13:18:10 -0400

    Base Game done

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ WHITE = (255, 255, 255)
 # Screen information
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
+SPEED=5
  
 DISPLAYSURF = pygame.display.set_mode((400,600))
 DISPLAYSURF.fill(WHITE)
@@ -31,13 +32,11 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0) 
  
       def move(self):
-        self.rect.move_ip(0,10)
+        self.rect.move_ip(0,SPEED)
         if (self.rect.bottom > 600):
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
  
-      def draw(self, surface):
-        surface.blit(self.image, self.rect) 
  
  
 class Player(pygame.sprite.Sprite):
@@ -47,7 +46,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (160, 520)
  
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
        #if pressed_keys[K_UP]:
             #self.rect.move_ip(0, -5)
@@ -61,8 +60,6 @@ class Player(pygame.sprite.Sprite):
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
  
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)     
  
          
 P1 = Player()
@@ -78,16 +75,27 @@ INC_SPEED = pygame.USEREVENT+1
 pygame.time.set_timer(INC_SPEED, 1000)
  
 while True:     
-    for event in pygame.event.get():              
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED+=2
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    P1.update()
-    E1.move()
-     
     DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    if pygame.sprite.spritecollideany(P1,enemies):
+        DISPLAYSURF.fill(RED)
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
          
     pygame.display.update()
     FramePerSec.tick(FPS)
