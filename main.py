85b3d114f051444c17f90c0c5ed5cbed49fb9df0
William Miller <wmilleiv@gmail.com>
2025-10-23 11:42:13 -0400

    Added second axis

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
  
 # Screen information
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
 SPEED=5
  
-DISPLAYSURF = pygame.display.set_mode((400,600))
+DISPLAYSURF = pygame.display.set_mode((1200,800))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
  
  
-class Enemy(pygame.sprite.Sprite):
+class EnemyTop(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
         self.image = pygame.image.load("./assets/Enemy.png")
@@ -37,7 +37,19 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
  
- 
+class EnemyLeft(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./assets/Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center =(0,random.randint(40,SCREEN_HEIGHT-40))
+
+    def move(self):
+        self.rect.move_ip(SPEED,0)
+        if(self.rect.right>800):
+            self.rect.left = 0
+            self.rect.center = random.randint(0,(30,570))
+
  
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -48,10 +60,10 @@ class Player(pygame.sprite.Sprite):
  
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-       #if pressed_keys[K_UP]:
-            #self.rect.move_ip(0, -5)
-       #if pressed_keys[K_DOWN]:
-            #self.rect.move_ip(0,5)
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0,5)
          
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
@@ -63,13 +75,16 @@ class Player(pygame.sprite.Sprite):
  
          
 P1 = Player()
-E1 = Enemy()
+E1 = EnemyTop()
+E2 = EnemyLeft()
 
 enemies = pygame.sprite.Group()
 enemies.add(E1)
+enemies.add(E2)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
+all_sprites.add(E2)
 
 INC_SPEED = pygame.USEREVENT+1
 pygame.time.set_timer(INC_SPEED, 1000)
